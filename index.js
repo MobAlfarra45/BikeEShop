9ec1439bf1ae1f59fd2a7ef293bd013079a25576
Mohammed Alfarra <alfarra@wisc.edu>
2023-11-11 17:58:48 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log(1);
+console.log(2);
+
+let age = 25;
+let year = 2023;
+
+console.log(age, year)
+
+age = 40;
+
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+var score = 75;
+console.log(score);
+
+let result = `template ${age} !`;
+
+let html = `<h2> ${points} </h2>`;
+
+let ninjas = ['alrayan', 'neko']; /* .join(''), creates string; .indexof(''); .concat([]), adds arrays together; .push('') adds new element; .pop() */
+
